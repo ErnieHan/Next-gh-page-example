ba93dd62618c47d42740572b63d1ba020f36b9be
葉宗翰 <02090560@me.mcu.edu.tw>
2020-04-17 03:23:32 +0800

    update css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Link from "next/link";
 import Head from "next/head";
+import styles from "../css/Header.module.css";
 
 export class App extends Component {
   render() {
@@ -18,7 +19,7 @@ export class App extends Component {
           <meta property="og:title" content="Taiwan Can Help" />
           <meta property="og:locale" content="zh-TW" /> */}
         </Head>
-        <h1>This is Ernie NEXT website to Github</h1>
+        <h1 className={styles.error}>This is Ernie NEXT website to Github</h1>
         <div className="menu-link">
           <Link href="/about" as={process.env.BACKEND_URL + "/about"}>
             <a>click me go to About Page</a>
